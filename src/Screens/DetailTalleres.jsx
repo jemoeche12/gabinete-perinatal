3e0b72f175fed0d17d8e6b91f76357797a14a96d
jeronimo <jemoeche@gmail.com>
2026-03-01 20:29:57 +0100

    arregloVideos

--- src/Screens/DetailTalleres.jsx
+++ src/Screens/DetailTalleres.jsx
@@ -6,7 +6,7 @@ import {
   ScrollView,
   Image,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { useGetTallerByIdQuery } from "../services/talleresService";
 import iconImageLoading from "../../assets/Red.png";
 import CustomHeader from "../components/CustomHeader";
@@ -15,10 +15,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { addCartItem } from "../features/cart/CartSlice";
 import back from "../../assets/icon/back.png";
 import { useVideoPlayer, VideoView } from "expo-video";
-import { getDownloadURL, getStorage, ref } from "firebase/storage";
 
 const VideoPlayer = ({ uri }) => {
-  const player = useVideoPlayer({ uri }, (player) => {
+  const player = useVideoPlayer( uri , (player) => {
     ((player.loop = false), (player.muted = false));
   });
 
@@ -37,37 +36,12 @@ const DetailTalleres = ({ route, navigation, visible }) => {
   const { tallerId } = route.params;
   const { data: taller, isLoading, error } = useGetTallerByIdQuery(tallerId);
   const [isMenuVisible, setIsMenuVisible] = useState(visible);
-  const [videoUrl, setVideoUrl] = useState({ url1: null, url2: null });
   const dispatch = useDispatch();
 
   const cartItems = useSelector((state) => state.cart.value.itemCart);
   const isCartEmpty = cartItems.length === 0;
 
-  useEffect(() => {
-    const fetchVideo = async () => {
-      try {
-        const storage = getStorage();
-        const promise = [
-          taller?.storagePath1
-            ? getDownloadURL(ref(storage, taller.storagePath1))
-            : Promise.resolve(null),
-          taller?.storagePath2
-            ? getDownloadURL(ref(storage, taller.storagePath2))
-            : Promise.resolve(null),
-        ];
-
-        const [url1, url2] = await Promise.all(promise);
-        setVideoUrl({ url1, url2 });
-      } catch (error) {
-        console.error(error);
-        setVideoUrl({ url1: null, url2: null });
-      }
-    };
-
-    if (taller?.storagePath1 || taller?.storagePath2) {
-      fetchVideo();
-    }
-  }, [taller]);
+  
 
   const handleAddItem = () => {
     dispatch(addCartItem({ ...taller }));
@@ -108,8 +82,8 @@ const DetailTalleres = ({ route, navigation, visible }) => {
         <Text style={styles.title}>{taller.titulo}</Text>
         <Text style={styles.description}>{taller.objetivo}</Text>
         <Text style={styles.contenidos}>{formatText(taller.contenidos)}</Text>
-        {videoUrl.url1 && <VideoPlayer uri={videoUrl.url1} />}
-        {videoUrl.url2 && <VideoPlayer uri={videoUrl.url2} />}
+        {taller.storagePath1 && <VideoPlayer uri={taller.storagePath1} />}
+        {taller.storagePath2 && <VideoPlayer uri={taller.storagePath2} />}
         <View style={styles.modalidad}>
           <Text style={styles.modalidadText}>Dia: {taller.modalidad.dia}</Text>
           <Text style={styles.modalidadText}>
